9f52f3ca20bad52d8e22853fadb986f1afe63e11
shandykate <shandy.kate@gmail.com>
2024-04-21 20:30:58 +0300

    Adds first files

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+function checkPassword(password) {
+  const correctPassword===password ?
+    return "Access granted" :
+    return "Access denied, wrong password!";
+}
+
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
+
+
